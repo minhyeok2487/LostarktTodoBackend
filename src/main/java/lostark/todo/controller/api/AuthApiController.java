f4c16819df097e4223bad55b17a29f214d0bc9b7
minhyeok2487 <repeater2487@naver.com>
2023-08-31 22:16:09 +0900

    오류 수정

--- src/main/java/lostark/todo/controller/api/AuthApiController.java
+++ src/main/java/lostark/todo/controller/api/AuthApiController.java
@@ -5,11 +5,17 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lostark.todo.config.TokenProvider;
+import lostark.todo.controller.dto.characterDto.CharacterResponseDto;
 import lostark.todo.controller.dto.memberDto.MemberResponseDto;
 import lostark.todo.controller.dto.memberDto.MemberDto;
 import lostark.todo.controller.dto.memberDto.MemberLoginDto;
 import lostark.todo.domain.character.Character;
+import lostark.todo.domain.content.DayContent;
+import lostark.todo.domain.market.Market;
 import lostark.todo.domain.member.Member;
+import lostark.todo.service.CharacterService;
+import lostark.todo.service.ContentService;
+import lostark.todo.service.MarketService;
 import lostark.todo.service.MemberService;
 import lostark.todo.service.lostarkApi.LostarkCharacterService;
 import org.springframework.http.HttpStatus;
@@ -20,7 +26,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -36,6 +44,9 @@ public class AuthApiController {
     private final MemberService memberService;
     private final LostarkCharacterService lostarkCharacterService;
     private final TokenProvider tokenProvider;
+    private final MarketService marketService;
+    private final ContentService contentService;
+    private final CharacterService characterService;
 
     @ApiOperation(value = "회원 가입",
             notes="대표캐릭터 검색을 통한 로스트아크 api 검증 \n 대표캐릭터와 연동된 캐릭터 함께 저장",
@@ -44,6 +55,17 @@ public class AuthApiController {
     public ResponseEntity signupMember(@RequestBody @Valid MemberDto memberDto) {
         // 대표캐릭터와 연동된 캐릭터(api 검증)
         List<Character> characterList = lostarkCharacterService.getCharacterList(memberDto);
+
+        // 재련재료 데이터 리스트로 거래소 데이터 호출
+        Map<String, Market> contentResource = marketService.getContentResource();
+
+        // 일일 숙제 통계 가져오기
+        Map<String, DayContent> dayContent = contentService.findDayContent();
+
+        for (Character character : characterList) {
+            // 일일숙제 예상 수익 계산(휴식 게이지 포함)
+            characterService.calculateDayTodo(character, contentResource, dayContent);
+        }
 
         // Member 회원가입
         Member signupMember = memberService.createMember(memberDto, characterList);
