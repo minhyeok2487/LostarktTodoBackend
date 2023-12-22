7f95ee7afecdb1528209fc56397314f8a0538cce
Minhyeok1995 <repeater2487@naver.com>
2023-12-22 17:05:14 +0900

    Refactor: 디스코드 웹훅 수정 - 임베디드 형식으로 디자인 변경 - 임베디드 형식을 변수로 받음

--- src/main/java/lostark/todo/service/WebHookService.java
+++ src/main/java/lostark/todo/service/WebHookService.java
@@ -1,6 +1,7 @@
 package lostark.todo.service;
 
 import lostark.todo.exhandler.exceptions.CustomIllegalArgumentException;
+import lostark.todo.service.discordWebHook.DiscordWebhook;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -9,40 +10,61 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.awt.*;
+import java.io.IOException;
+
 @Service
 public class WebHookService {
-    @Value("${discord.webhookURL}")
-    private  String url;
 
-    public void callEvent(Exception ex){
+    private String url = "https://discord.com/api/webhooks/1186118216452427776/FkY1W_sn60sZNyxGS5yjTzazuDL6x3op6kNkVeNXKamW1Kp5_Q7BoXdGxoBedcRFY3FQ";
+
+    @Value("${discord.noticeURL}")
+    private String noticeUrl;
+
+    public void sendMessage(DiscordWebhook.EmbedObject embedObject) {
+        DiscordWebhook webhook = new DiscordWebhook(noticeUrl);
+        webhook.setUsername("Loatodo 알림");
+        webhook.addEmbed(embedObject);
+        try {
+            webhook.execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void callEvent(Exception ex) {
         JSONObject data = new JSONObject();
         String message = "```" + ex.getClass().getSimpleName() + "발생";
         message += "\n";
         message += ex.getMessage();
         message += "```";
         data.put("content", message);
-        send(data);
+        send(data, url);
     }
 
-    public void callEvent(CustomIllegalArgumentException ex){
+    public void callEvent(CustomIllegalArgumentException ex) {
+        StackTraceElement[] stackTrace = ex.getStackTrace();
+        for (StackTraceElement stackTraceElement : stackTrace) {
+            System.out.println("stackTraceElement = " + stackTraceElement);
+        }
         JSONObject data = new JSONObject();
-        String message = "``` [" + ex.getMethod()+ "]  발생";
+        String message = "``` [" + ex.getMethod() + "]  발생";
         message += "\n";
-        if(ex.getMember() != null) {
+        if (ex.getMember() != null) {
             message += ex.getMember().getId() + " / " + ex.getMember().getUsername() + " / ";
         }
         message += ex.getMessage();
         message += "```";
         data.put("content", message);
-        send(data);
+        send(data, url);
     }
 
-    private void send(JSONObject object){
+    private void send(JSONObject object, String webhookUrl) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<String> entity = new HttpEntity<>(object.toString(), headers);
-        restTemplate.postForObject(url, entity, String.class);
+        restTemplate.postForObject(webhookUrl, entity, String.class);
     }
 }
