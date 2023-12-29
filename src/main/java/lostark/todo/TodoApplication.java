fd34b6f3e13e6ea3fa3ee21c8158c0ddbcf189ea
Minhyeok1995 <repeater2487@naver.com>
2023-12-29 21:25:49 +0900

    Fetch: 프로메테우스 테스트

--- src/main/java/lostark/todo/TodoApplication.java
+++ src/main/java/lostark/todo/TodoApplication.java
@@ -1,7 +1,9 @@
 package lostark.todo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -16,4 +18,9 @@ public class TodoApplication {
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
+
+	@Bean
+	public InMemoryHttpTraceRepository traceRepository() {
+		return new InMemoryHttpTraceRepository();
+	}
 }
