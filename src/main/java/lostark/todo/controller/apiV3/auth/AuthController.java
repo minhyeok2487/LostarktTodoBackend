dccccc840dda8e9a5484b08ef5b05d0815d67b7f
Minhyeok1995 <repeater2487@naver.com>
2024-01-01 08:45:09 +0900

    Fetch: 1차 회원가입 이후 캐릭터 추가 API

--- src/main/java/lostark/todo/controller/apiV3/auth/AuthController.java
+++ src/main/java/lostark/todo/controller/apiV3/auth/AuthController.java
@@ -6,16 +6,26 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lostark.todo.controller.dto.auth.AuthResponseDto;
 import lostark.todo.controller.dto.auth.AuthSignupDto;
+import lostark.todo.controller.dto.memberDto.MemberRequestDto;
+import lostark.todo.controller.dto.memberDto.MemberResponseDto;
+import lostark.todo.domain.character.Character;
+import lostark.todo.domain.content.Category;
+import lostark.todo.domain.content.DayContent;
+import lostark.todo.domain.market.Market;
 import lostark.todo.domain.member.Member;
-import lostark.todo.service.AuthService;
-import lostark.todo.service.MailService;
-import lostark.todo.service.MemberService;
+import lostark.todo.service.*;
+import lostark.todo.service.lostarkApi.LostarkApiService;
+import lostark.todo.service.lostarkApi.LostarkCharacterService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @Slf4j
@@ -27,6 +37,11 @@ public class AuthController {
     private final MemberService memberService;
     private final AuthService authService;
     private final MailService mailService;
+    private final CharacterService characterService;
+    private final MarketService marketService;
+    private final ContentService contentService;
+    private final LostarkCharacterService lostarkCharacterService;
+    private final ConcurrentHashMap<String, Boolean> usernameLocks;
 
     @ApiOperation(value = "1차 회원 가입",
             notes="이메일, 비밀번호(O), Api-Key, 대표캐릭터(X)", response = AuthResponseDto.class)
@@ -50,6 +65,43 @@ public class AuthController {
         // 회원가입 완료시 Redis에 저장된 인증번호 모두 삭제
         mailService.deleteAll(authSignupDto.getMail());
         return new ResponseEntity<>(new AuthResponseDto(true, message), HttpStatus.CREATED);
+    }
+
+    @ApiOperation(value = "1차 회원가입 이후 캐릭터 추가",
+            notes="대표캐릭터 검색을 통한 로스트아크 api 검증 \n 대표캐릭터와 연동된 캐릭터 함께 저장",
+            response = MemberResponseDto.class)
+    @PostMapping("/character")
+    public ResponseEntity<?> saveCharacter(@RequestBody @Valid MemberRequestDto memberDto) {
+        if (usernameLocks.putIfAbsent(memberDto.getUsername(), true) != null) {
+            throw new IllegalStateException("이미 진행중입니다.");
+        }
+        try {
+            // 일일 컨텐츠 통계(카오스던전, 가디언토벌) 호출
+            List<DayContent> chaos = contentService.findDayContent(Category.카오스던전);
+            List<DayContent> guardian = contentService.findDayContent(Category.가디언토벌);
+
+            // 대표캐릭터와 연동된 캐릭터 호출(api 검증)
+            List<Character> characterList = lostarkCharacterService.findCharacterList(memberDto.getCharacterName(), memberDto.getApiKey(), chaos, guardian);
+
+            // 재련재료 데이터 리스트로 거래소 데이터 호출
+            Map<String, Market> contentResource = marketService.findContentResource();
+
+            // 일일숙제 예상 수익 계산(휴식 게이지 포함)
+            List<Character> calculatedCharacterList = new ArrayList<>();
+            for (Character character : characterList) {
+                Character result = characterService.calculateDayTodo(character, contentResource);
+                calculatedCharacterList.add(result);
+            }
+
+            // Member 회원가입
+            Member signupMember = memberService.createCharacter(memberDto.getUsername(), memberDto.getApiKey(), calculatedCharacterList);
+
+            // 결과 출력
+            MemberResponseDto memberResponseDto = new MemberResponseDto().toDto(signupMember);
+            return new ResponseEntity<>(memberResponseDto, HttpStatus.OK);
+        } finally {
+            usernameLocks.remove(memberDto.getUsername());
+        }
     }
 
     @ApiOperation(value = "로그아웃",
