19c92a6b33484eddc8a9c0d5caa38ad43d5a6749
Minhyeok1995 <repeater2487@naver.com>
2024-04-07 14:48:54 +0900

    Fix : 코드정리

--- src/main/java/lostark/todo/domain/notification/NotificationRepository.java
+++ src/main/java/lostark/todo/domain/notification/NotificationRepository.java
@@ -1,8 +1,5 @@
 package lostark.todo.domain.notification;
 
-import lostark.todo.domain.notices.Notices;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
