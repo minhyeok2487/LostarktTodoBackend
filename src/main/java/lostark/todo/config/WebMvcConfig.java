abe477a8aedf94eb86160a8eda3fe32c280d4dc3
Minhyeok1995 <repeater2487@naver.com>
2024-06-01 06:22:40 +0900

    Fix : 롤백

--- src/main/java/lostark/todo/config/WebMvcConfig.java
+++ src/main/java/lostark/todo/config/WebMvcConfig.java
@@ -16,7 +16,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
         // 모든 경로에 대하여
         registry.addMapping("/**")
                 // Origin이 http:localhost:3000에 대해.
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000", // 로컬
+                        "https://app.loatodo.com/", "https://loatodo.com", "https://www.loatodo.com", "http://192.168.35.174:3000", "http://210.221.60.15:3000",
+                        "https://dev.loatodo.com")
                 // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .allowedHeaders("*")
