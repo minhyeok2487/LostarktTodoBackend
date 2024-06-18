f78c715b5cb79b070e7626d049bf8aaadecd86c0
Minhyeok1995 <repeater2487@naver.com>
2024-06-18 19:20:29 +0900

    CORS 모든 도메인 허용

--- src/main/java/lostark/todo/config/WebMvcConfig.java
+++ src/main/java/lostark/todo/config/WebMvcConfig.java
@@ -13,17 +13,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // 모든 경로에 대하여
         registry.addMapping("/**")
-                // Origin이 http:localhost:3000에 대해.
-                .allowedOrigins("http://localhost:3000", // 로컬
-                        "https://app.loatodo.com/", "https://loatodo.com", "https://www.loatodo.com", "http://192.168.35.174:3000", "http://210.221.60.15:3000",
-                        "https://dev.loatodo.com")
-                // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
+                .allowedOriginPatterns("*")
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(MAX_AGE_SECS);
     }
-
 }
