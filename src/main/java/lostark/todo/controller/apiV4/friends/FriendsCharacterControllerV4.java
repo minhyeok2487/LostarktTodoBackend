55e24f67cb0e0b1f2dd69733dd695bacadc296b9
Minhyeok1995 <repeater2487@naver.com>
2024-09-03 11:31:53 +0900

    ADD : 깐부 캐릭터 레이드 골드 체크 방식 업데이트 API

--- src/main/java/lostark/todo/controller/apiV4/friends/FriendsCharacterControllerV4.java
+++ src/main/java/lostark/todo/controller/apiV4/friends/FriendsCharacterControllerV4.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lostark.todo.controller.dto.characterDto.CharacterDefaultDto;
+import lostark.todo.controller.dto.characterDto.CharacterDto;
 import lostark.todo.controller.dtoV2.character.CharacterResponse;
 import lostark.todo.controller.dtoV2.character.UpdateMemoRequest;
 import lostark.todo.domain.character.Character;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+import static lostark.todo.constants.ErrorMessages.CHARACTER_NOT_FOUND;
 import static lostark.todo.constants.ErrorMessages.FRIEND_PERMISSION_DENIED;
 
 
@@ -31,7 +33,32 @@ public class FriendsCharacterControllerV4 {
     private final CharacterService characterService;
     private final FriendsService friendsService;
 
-    @ApiOperation(value = "골드 획득 캐릭터 지정/해제", response = CharacterResponse.class)
+    @ApiOperation(value = "깐부 캐릭터 레이드 골드 체크 방식 업데이트", response = CharacterDto.class)
+    @PatchMapping("/{friendUsername}/gold-check-version")
+    public ResponseEntity<CharacterDto> updateDayTodoCheck(
+            @AuthenticationPrincipal String username,
+            @RequestBody @Valid CharacterDefaultDto characterDefaultDto,
+            @PathVariable String friendUsername) {
+
+        Friends friend = friendsService.findByFriendUsername(friendUsername, username);
+
+        if (!friend.getFriendSettings().isSetting()) {
+            throw new IllegalArgumentException(FRIEND_PERMISSION_DENIED);
+        }
+
+        Character character = friend.getMember().getCharacters().stream()
+                .filter(c -> c.getId() == characterDefaultDto.getCharacterId())
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(CHARACTER_NOT_FOUND));
+
+        Character updatedCharacter = characterService.updateGoldCheckVersion(character);
+        CharacterDto characterDto = new CharacterDto().toDtoV2(updatedCharacter);
+
+        return new ResponseEntity<>(characterDto, HttpStatus.OK);
+    }
+
+
+    @ApiOperation(value = "깐부 골드 획득 캐릭터 지정/해제", response = CharacterResponse.class)
     @PatchMapping("/{friendUsername}/gold-character")
     public ResponseEntity<CharacterResponse> updateGoldCharacter(@AuthenticationPrincipal String username,
                                                                  @RequestBody CharacterDefaultDto characterDefaultDto,
