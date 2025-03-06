91c2a3547b9cd482cc30d1dacdce1fd87c426304
Minhyeok1995 <repeater2487@naver.com>
2025-03-06 18:26:01 +0900

    fix: 로그 저장 TaskExecutor로 변경

--- src/main/java/lostark/todo/global/config/OtherConfig.java
+++ src/main/java/lostark/todo/global/config/OtherConfig.java
@@ -3,10 +3,14 @@ package lostark.todo.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,5 +26,18 @@ public class OtherConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Primary
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5); // 기본 스레드 수
+        executor.setMaxPoolSize(10); // 최대 스레드 수
+        executor.setQueueCapacity(100); // 대기 큐 크기
+        executor.setThreadNamePrefix("LogAsync-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy()); // 큐 꽉 차면 호출자 스레드 실행
+        executor.initialize();
+        return executor;
     }
 }
