53ac5ee9ffb10d3f350b4427c1c9242633964519
Minhyeok1995 <repeater2487@naver.com>
2025-03-11 21:34:06 +0900

    feat: Add username index in Member entity

--- src/main/java/lostark/todo/domain/member/entity/Member.java
+++ src/main/java/lostark/todo/domain/member/entity/Member.java
@@ -23,6 +23,7 @@ import java.util.Objects;
 @AllArgsConstructor
 @Builder
 @Entity
+@Table(name = "member", indexes = @Index(name = "idx_username", columnList = "username", unique = true))
 public class Member extends BaseTimeEntity {
 
     // 회원 테이블
