bc1efc74d9955f5fff6b06bdb41daea798366989
Minhyeok1995 <repeater2487@naver.com>
2025-07-10 09:40:08 +0900

    feat: 생활의 기운 캐릭터 삭제 api 추가

--- src/main/java/lostark/todo/domain/member/repository/LifeEnergyRepository.java
+++ src/main/java/lostark/todo/domain/member/repository/LifeEnergyRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface LifeEnergyRepository extends JpaRepository<LifeEnergy, Long> {
+
+    Optional<LifeEnergy> findByMemberUsernameAndCharacterName(String username, String characterName);
 
     @Modifying
     @Query("UPDATE LifeEnergy le SET le.energy = " +
